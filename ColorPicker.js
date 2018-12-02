560a3e3af64f5991a0ea9de5fb33f8746f50bffa
alixivelasco <alixivelasco@gmail.com>
2018-12-02 23:32:27 +0000

    Updated 02.12.18

--- ColorPicker.js
+++ ColorPicker.js
@@ -1,15 +1,23 @@
 var colorThief = new ColorThief();
 
-var sourceImage = document.getElementById("image");
+var sourceImage = document.createElement("img");
+sourceImage.src = "sample-teal.png"
 
-// Display main color
-// e.g [125, 189, 193]
-console.log(
-    colorThief.getColor(sourceImage)
-);
+var image = document.getElementById("image");
+image.appendChild(sourceImage)
 
-// Display palette of colors
-// e.g [[55,37,29],[213,193,136],[110,204,223]]
-console.log(
-    colorThief.getPalette(sourceImage)
-);
+//get dominant colour from image
+var dominantColour = colorThief.getColor(sourceImage);
+
+//RGB colours to match images to, which will then correspond to SKU's
+var teal = [7,101,110];
+var grey = [62,71,87];
+
+if (_.isEqual (teal, dominantColour)) {
+	document.getElementById("outPutColour").innerHTML = "Teal - SKU7101110";
+}
+else if(_.isEqual (grey, dominantColour)){
+	document.getElementById("outPutColour").innerHTML = "Grey - SKU627187";
+}
+else {console.log("The colour is not recognised")}
+
